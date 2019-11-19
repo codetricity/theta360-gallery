f2e7cb5d06ef6546f390cf976398bb2189dda1f4
Craig <craig@oppkey>
2019-11-19 12:48:43 -0800

    decreased font size of cards

--- jobs/models.py
+++ jobs/models.py
@@ -4,14 +4,14 @@ from django.db import models
 class Job(models.Model):
     order = models.PositiveIntegerField()
     image = models.ImageField(upload_to='images/')
-    title = models.CharField(max_length=80)
-    summary = models.CharField(max_length=200)
-    camera_model = models.CharField(max_length=120)
+    title = models.CharField(max_length=40)
+    summary = models.CharField(max_length=80)
+    camera_model = models.CharField(max_length=30)
     level = models.CharField(max_length=30)
     photographer = models.CharField(max_length=70, default='community')
     photo_studio = models.CharField(max_length=70, default='individual')
     more_info_url = models.CharField(max_length=200)
-    technique = models.CharField(max_length=200)
+    technique = models.CharField(max_length=80)
 
     def __str__(self):
         return str(self.order) + self.title
