c299a9520973ac179ae3ebb700f84ff697e06e45
Craig <craig@oppkey>
2019-11-21 10:42:54 -0800

    added opengraph and Twitter meta tags

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
+from jobs.models import Job
 
 
 def allblogs(request):
@@ -14,7 +15,14 @@ def qualitytuba(request):
 
 def detail(request, blog_id):
     detailblog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog': detailblog})
+    job = Job.objects.get(detail_page=detailblog)
+    return render(
+        request,
+        'blog/detail.html',
+        {
+            'blog': detailblog,
+            'job': job
+        })
 
 
 def about(request):
